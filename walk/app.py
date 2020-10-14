5bcf132ca834e80e1c79b687975c72c3190ce748
Graham Livingston <gramlivingston@gmail.com>
2020-10-14 14:33:00 -0500

    added webscraping to index

--- walk/app.py
+++ walk/app.py
@@ -21,11 +21,15 @@ def index():
     usa = 'usa'
 
 
+
     #get number
     number = 20
-    
+
+
+
+
     #move through list
-    search = gallery
+    search = UIC
     article = []
     results = 100 # valid options 10, 20, 30, 40, 50, and 100
     page = requests.get(f"https://www.google.com/search?q={search}&num={results}")
