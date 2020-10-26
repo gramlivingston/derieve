264f36695188c3cefc12a0437ed4cd0fc6fb8ec3
Graham Livingston <gramlivingston@gmail.com>
2020-10-26 10:55:11 -0500

    saved app.py and fixed body tag html

--- walk/app.py
+++ walk/app.py
@@ -1,6 +1,6 @@
 #add code for flask app object
 from flask import Flask, render_template, jsonify
-import time
+# import time
 
 app = Flask(__name__)
 
@@ -8,58 +8,48 @@ app = Flask(__name__)
 from bs4 import BeautifulSoup
 import requests
 import re
-from scraper_api import ScraperAPIClient
+# from scraper_api import ScraperAPIClient
 
 
 #set route for user navigation
-@app.route('/_stuff')
-
-#define app function
-def stuff():
-    
-        #Set up list
-    gallery = "gallery&400"
-    UIC = 'UIC'
-    chicago = 'chicago'
-    usa = 'usa'
-
-
-    #set headers
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    #get number
-    number = 40
-
-
-
-
-    #move through list
-    search = UIC
-    article = []
-    results = 100 # valid options 10, 20, 30, 40, 50, and 100
-    page = requests.get(f"https://www.google.com/search?q={search}&num={results}&pws=0",headers = headers)
-    
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.findAll("a")
-    for link in links :
-        link_href = link.get('href')
-        if "url?q=" in link_href and not "webcache" in link_href:
-            article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
-
-    time.sleep(10)
-    page = requests.get(f'{article[number]}', headers = headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    text = soup.find('p').getText()
-    time.sleep(40)
-    print(text)
-    
-    return jsonify(result=text)
 
 @app.route('/')
 
 #define app function
-def index():
+    def index():
 
-    return render_template("index.html")
+        #Set up list
+        gallery = "gallery&400"
+        UIC = 'UIC'
+        chicago = 'chicago'
+        usa = 'usa'
+
+
+        #set headers
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        #get number
+        number = 40
+
+
+
+
+        #move through list
+        search = UIC
+        article = []
+        results = 100 # valid options 10, 20, 30, 40, 50, and 100
+        page = requests.get(f"https://www.google.com/search?q={search}&num={results}&pws=0",headers = headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        links = soup.findAll("a")
+        for link in links :
+            link_href = link.get('href')
+            if "url?q=" in link_href and not "webcache" in link_href:
+                article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
+
+        page = requests.get(f'{article[number]}', headers = headers)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        text = soup.find('p').getText()
+
+        return render_template("index.html", text = text)
 
 #set route for user navigation
 @app.route('/uic')
