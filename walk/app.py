62be529e00d099f99df7dea8a66ed447bb1b7c10
Graham Livingston <gramlivingston@gmail.com>
2020-10-26 11:36:48 -0500

    wikipedia search

--- walk/app.py
+++ walk/app.py
@@ -19,9 +19,9 @@ import re
 def index():
 
     #Set up list
-    gallery = "gallery&400"
-    UIC = 'UIC'
-    chicago = 'chicago'
+    gallery = "Gallery_400"
+    UIC = 'University_of_Illinois_at_Chicago'
+    chicago = 'Chicago'
     usa = 'usa'
 
 
@@ -36,19 +36,11 @@ def index():
         #move through list
     search = UIC
     article = []
-    results = 100 # valid options 10, 20, 30, 40, 50, and 100
-    
-    page = requests.get(f"https://en.wikipedia.org/wiki/{search}",headers = headers)
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.findAll("a")
-    for link in links :
-        link_href = link.get('href')
-        if "url?q=" in link_href and not "webcache" in link_href:
-            article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
 
-    page = requests.get(f'{article[number]}', headers = headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    text = soup.find('p').getText()
+    page = requests.get(f'https://en.wikipedia.org/wiki/{search}', headers = headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    text = soup.text
+    print (text[500:800])
 
     return render_template("index.html", text = text)
 
@@ -58,9 +50,9 @@ def index():
 #define app function
 def UIC():
     #Set up list
-    gallery = "gallery&400"
-    UIC = 'UIC'
-    chicago = 'chicago'
+    gallery = "Gallery_400"
+    UIC = 'University_of_Illinois_at_Chicago'
+    chicago = 'Chicago'
     usa = 'usa'
 
 
@@ -72,21 +64,14 @@ def UIC():
 
 
 
-    #move through list
+        #move through list
     search = UIC
     article = []
-    results = 100 # valid options 10, 20, 30, 40, 50, and 100
-    page = requests.get(f"https://www.google.com/search?q={search}&num={results}&pws=0",headers = headers)
-    soup = BeautifulSoup(page.content, "html.parser")
-    links = soup.findAll("a")
-    for link in links :
-        link_href = link.get('href')
-        if "url?q=" in link_href and not "webcache" in link_href:
-            article.append((link.get('href').split("?q=")[1].split("&sa=U")[0]))
 
-    page = requests.get(f'{article[number]}', headers = headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    text = soup.find('p').getText()
+    page = requests.get(f'https://en.wikipedia.org/wiki/{search}', headers = headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    text = soup.text
+    print (text[500:800])
 
     return render_template("index.html", text = text)
 
